f5f0e647692a88781bbcc60c0cdbbea3f4caac7d
Dijets <39377778+lasthyphen@users.noreply.github.com>
2021-06-11 15:42:15 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Checkout from './components/Checkout';
+import Success from './components/Success';
+import Canceled from './components/Canceled';
+
+import './css/normalize.css';
+import './css/global.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/success.html">
+          <Success />
+        </Route>
+        <Route path="/canceled.html">
+          <Canceled />
+        </Route>
+        <Route path="/">
+          <Checkout />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
