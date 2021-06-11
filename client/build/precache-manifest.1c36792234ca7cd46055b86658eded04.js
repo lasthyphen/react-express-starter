b247d486de848b8ccb74de7b1b4f911e1f0008a8
Dijets <39377778+lasthyphen@users.noreply.github.com>
2021-06-11 17:02:49 +0100

    Add files via upload

---
+++ client/build/precache-manifest.1c36792234ca7cd46055b86658eded04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36423adba34f178351bb066957122ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a349cf0c3c188c869c2d",
+    "url": "/static/css/main.83a3560a.chunk.css"
+  },
+  {
+    "revision": "78f35220134518e67867",
+    "url": "/static/js/2.f96c4dc1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f96c4dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a349cf0c3c188c869c2d",
+    "url": "/static/js/main.7d74d6e3.chunk.js"
+  },
+  {
+    "revision": "8e42e4e40bbbee2dca6a",
+    "url": "/static/js/runtime-main.c7bad3dc.js"
+  },
+  {
+    "revision": "542daa7a947232d48bf7c03ebaad214f",
+    "url": "/static/media/daggu4.542daa7a.png"
+  },
+  {
+    "revision": "b97ec1b590f53d443f8e383b3c34df1d",
+    "url": "/static/media/logo23.b97ec1b5.png"
+  },
+  {
+    "revision": "87fe06b6369394012b81ed06c2793eed",
+    "url": "/static/media/tumri2.87fe06b6.gif"
+  }
+]);
